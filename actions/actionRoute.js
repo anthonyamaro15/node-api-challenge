58b8781d62d45dedd08fb693aa3a227d03011bb3
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-08 10:09:59 -0700

    added asyn await

--- actions/actionRoute.js
+++ actions/actionRoute.js
@@ -4,49 +4,47 @@ const db = require("../data/helpers/actionModel");
 const route = express.Router();
 
 //GET /api/actions
-route.get("/", (req, res) => {
-  db.get()
-    .then((actions) => {
-      res.status(200).json(actions);
-    })
-    .catch((err) => {
-      res.json({ errorMessage: "there was an error getting the actions" });
-    });
+route.get("/", async (req, res) => {
+  try {
+    const actions = await db.get();
+    res.status(200).json(actions);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ errorMessage: "there was an error getting the actions" });
+  }
 });
 
 // POST /api/actions
-route.post("/", (req, res) => {
-  db.insert(req.body)
-    .then((action) => {
-      res.status(201).json(action);
-    })
-    .catch((err) => {
-      res.json({ errorMessage: "there was an error adding the action" });
-    });
+route.post("/", async (req, res) => {
+  try {
+    const action = await db.insert(req.body);
+    res.status(201).json(action);
+  } catch (error) {
+    res.json({ errorMessage: "there was an error adding the action" });
+  }
 });
 
 // PUT /api/actions/:id
-route.put("/:id", (req, res) => {
-  const { id } = req.params;
-  db.update(id, req.body)
-    .then((action) => {
-      res.status(200).json(action);
-    })
-    .catch((err) => {
-      res.json({ errorMessage: "there was an error updating the action" });
-    });
+route.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const action = await db.update(id, req.body);
+    res.status(200).json(action);
+  } catch (error) {
+    res.json({ errorMessage: "there was an error updating the action" });
+  }
 });
 
 // DELETE /api/actions/:id
-route.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  db.remove(id, req.body)
-    .then((action) => {
-      res.status(200).json(action);
-    })
-    .catch((err) => {
-      res.json({ errorMessage: "there was an error deleting the action" });
-    });
+route.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const action = await db.remove(id);
+    res.status(200).json(action);
+  } catch (error) {
+    res.json({ errorMessage: "there was an error deleting the action" });
+  }
 });
 
 module.exports = route;
--- projects/projectRoute.js
+++ projects/projectRoute.js
@@ -4,69 +4,62 @@ const db = require("../data/helpers/projectModel");
 const route = express.Router();
 
 // GET /api/projects
-route.get("/", (req, res) => {
-  db.get()
-    .then((projects) => {
-      res.status(200).json(projects);
-    })
-    .catch((err) => {
-      res
-        .status(500)
-        .json({ errorMessage: "there was an error gettting the projects" });
-    });
+route.get("/", async (req, res) => {
+  try {
+    const projects = await db.get();
+    res.status(200).json(projects);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ errorMessage: "there was an error gettting the projects" });
+  }
 });
 
 // GET /api/projects/:id/actions
-route.get("/:id/actions", (req, res) => {
-  const { id } = req.params;
-  db.getProjectActions(id)
-    .then((actions) => {
-      res.status(200).json(actions);
-    })
-    .catch((err) => {
-      res
-        .status(500)
-        .json({
-          errorMessage: "there was an error getting actions for this project",
-        });
+route.get("/:id/actions", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const actions = await db.getProjectActions(id);
+    res.status(200).json(actions);
+  } catch (error) {
+    res.status(500).json({
+      errorMessage: "there was an error getting actions for this project",
     });
+  }
 });
 
 // POST /api/projects
-route.post("/", (req, res) => {
-  db.insert(req.body)
-    .then((project) => {
-      res.status(201).json(project);
-    })
-    .catch((err) => {
-      res
-        .status(500)
-        .json({ errorMessage: "there was an error adding a project" });
-    });
+route.post("/", async (req, res) => {
+  try {
+    const project = await db.insert(req.body);
+    res.status(201).json(project);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ errorMessage: "there was an error adding a project" });
+  }
 });
 
 // PUT /api/projects/:id
-route.put("/:id", (req, res) => {
-  const { id } = req.params;
-  db.update(id, req.body)
-    .then((project) => {
-      res.status(200).json(project);
-    })
-    .catch((err) => {
-      res.json({ errorMessage: "there was an error updating a project" });
-    });
+route.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const project = await db.update(id, req.body);
+    res.status(200).json(project);
+  } catch (error) {
+    res.json({ errorMessage: "there was an error updating a project" });
+  }
 });
 
 // DELETE /api/projects/:id
-route.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  db.remove(id)
-    .then((project) => {
-      res.status(200).json(project);
-    })
-    .catch((err) => {
-      res.json({ errorMessage: "there was an error removing a project" });
-    });
+route.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const project = await db.remove(id);
+    res.status(200).json(project);
+  } catch (error) {
+    res.json({ errorMessage: "there was an error removing a project" });
+  }
 });
 
 module.exports = route;
