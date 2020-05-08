450a60a0130b3d8832fc16d03758509e1bc9e691
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-08 09:54:45 -0700

    finish

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const projectRouter = require("../projects/projectRoute");
 const actionRouter = require("../actions/actionRoute");
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+
 server.use("/api/projects", projectRouter);
 server.use("/api/actions", actionRouter);
 
